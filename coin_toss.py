153de0beaaed7a769e5bb5e2459de4af51a32327
mcflcc <mcintron2@fingerlakes.edu>
2026-03-02 02:23:54 +0000

    Pasted my coin_toss.py code

--- coin_toss.py
+++ coin_toss.py
@@ -1,14 +1,33 @@
 # FILE NAME - coin_toss.py
-# NAME: 
-# DATE: 
-# BRIEF DESCRIPTION:  
+
+# NAME: Mike Cintron
+# DATE: 3.1.2026
+# BRIEF DESCRIPTION:  Code that creates a random number - if the number is 51 or greater - then tails - else heads
+
+
+
 # 1. Make sure you fill out the comments above
 # 2. Write your code in the proper spot
 # 3. Be sure to answer the Reflection Questions and Attestation below
 # 4. The Sample Output has been included in this code for your convenience
+
+
+
 ########## ENTER YER CODE BELOW THIS LINE ##########
 
+# Don't forget to import random!!!!!
 
+import random
+
+random_number = random.randint(1,100)
+
+if random_number < 51:
+    print("===== Coin Flipper =====")
+    print("Heads")
+
+else:
+    print("===== Coin Flipper =====")
+    print("Tails")
 
 
 
@@ -18,6 +37,9 @@
 
 
 ########### END YER CODE ABOVE THIS LINE ###########
+
+    
+
 
 
 ########################################
@@ -43,7 +65,7 @@ Tails
 '''
 
 1. What was the hardest part of completing this lab? 
-
+Hardest part was getting the conditional correct for the if statement.
 
 
 
@@ -63,5 +85,5 @@ may impede your understanding. Please rate how well you understand the concepts 
 [ ] I understand very little about this lab.
 [ ] I am about 50/50 on this lab; I get parts of it but not the whole picture.
 [ ] I pretty much get it.
-[ ] I'm solid. Totally got it.
+[ x] I'm solid. Totally got it.
 '''
